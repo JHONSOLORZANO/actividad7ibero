3eb2be300b149ea813dccf69efd4dc26c5c822eb
JHONSOLORZANO <jhonsolorzanorojas@gmail.com>
2020-08-08 14:01:25 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    /* Script ReaccionMouse1 */
+    
+        $(".ReaccionMouse1").mouseover(function(){
+    
+            $(this).css("background", "blue").css("color","white");
+        
+        });
+    
+        $(".ReaccionMouse1").mouseout(function(){
+    
+            $(this).css("background", "White").css("color","white");
+        
+        });
+    
+    });
+
+    
